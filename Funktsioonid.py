fa02846248fa4cbb8230acb0188c7aa447b2d7e8
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2021-11-19 11:00:48 +0200

    Registreerimine

--- Funktsioonid.py
+++ Funktsioonid.py
@@ -14,3 +14,16 @@ def passautomat()->str:
 	psword = ''.join([random.choice(ls) for x in range(12)])
 # Пароль готов
 	return psword
+def paskontroll(psword: str)->bool:
+	ls=list(psword)
+	for e in ls:
+		if e.isdigit(): d=True
+		if e.isalpha(): a=True
+		if e.isupper(): u=True
+		if e.islower():l=True
+		if e in list(".,:;!_*-+()/#¤%&"): s=True
+	if d==True and a==True and u==True and l==True and s==True:
+		t=True
+	else:
+		t=False
+	return t
--- KasutajadSalasonad.py
+++ KasutajadSalasonad.py
@@ -2,5 +2,37 @@ from Funktsioonid import *
 users=["Marina"]
 passwords=["12345"]
 
-password=passautomat()
-print(password) 
+while True:
+	print("Reg-1,Avt-2,Välja-3")
+	v=int(input())
+	if v==1:
+		print("Registreerimine")
+		while 1:
+			log=input("Kasutajatunnus")
+			if log not in users:
+				print("Tunnus soobib")
+				break
+			else:
+				print("See nimi juba on olemas listis")	
+		v=input("Arvuti-A või ise-I loob parool")
+		if v.upper()=="A":
+			pas=passautomat()
+		elif v.upper()=="I":			
+			while 1:
+				pas=input("Sisesta oma parool")
+				tulemus=paskontroll(pas)
+				if tulemus==True:
+					users.append(log)
+					passwords.append(pas)
+					break
+
+		
+	elif v==2:
+		print("Avtoriseerimine")
+		#
+	elif v==3:
+		print("Välja")
+		break
+		#valmis
+	else:
+		print("On vaja valida 1,2 või 3")# kõik on olemas
